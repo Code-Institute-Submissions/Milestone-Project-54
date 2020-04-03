8d4ceac2ab66d98dca70f4ae4319dbb19b387b20
rimantascode <r.preiks@gmail.com>
2020-04-03 18:20:07 +0000

    still working on basic layout, slider is working with few bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
- 
-$("header").mousemove(function( event ) {
+    var mainTimer = null;
 
-});
-    
+
     var a = [];
     var imageIndex = 0;
     var i = 0;
+
     showImages();
     function showImages() {
         while (a.length < 3) {
@@ -23,7 +22,44 @@ $("header").mousemove(function( event ) {
         }
         $(a[imageIndex]).fadeIn(2000).fadeOut(2000);
         // $(a[0]).fadeIn(2000);
-        setTimeout(showImages,4000);
-      
+
+       mainTimer =setTimeout(showImages, 4000);
     }
+    
+  /*
+    $("header").mousemove(function (event) {
+        var movement= 100;
+       var movement=movement-event.PageX
+       
+        if (event.PageX == true ) {
+         
+              $(a[imageIndex]).stop();
+              $(a[imageIndex]).fadeIn();
+       console.log("moves");
+
+        }else {
+          // setInterval(function(){ console.log("new"+movement); }, 3000);
+        }
+       
+    });
+    */
+   var timeout = null;
+
+$(document).on('mousemove', function() {
+    clearTimeout(timeout);
+      $(a[imageIndex]).stop().fadeIn(1000);
+    timeout = setTimeout(function() {
+        console.log('Mouse idle for 3 sec');
+        clearTimeout(mainTimer);
+        showImages();
+    }, 3000);
+});
+$( window ).resize(function() {
+    if ($( window ).width() < 1300) {
+        $( ".buttons-container div" ).css("width", "200" );
+        $( ".buttons-container div" ).css("height", "200" );
+    }
+  
+  console.log($( window ).width());
+});
 });
