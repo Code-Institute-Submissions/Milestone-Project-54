34476149c98a4a1aa5d7214c490ad7843a5f6629
rimantascode <r.preiks@gmail.com>
2020-04-10 16:02:39 +0000

    worked on the layout for the smaller screens it still have some bugs,like slider for some reason jumps to other section, and when the slider have start from the first it takes a bit of time and the screen is blank. leaving that for latter.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,8 @@ $(document).ready(function () {
         }
         if (imageIndex < a.length) {
             imageIndex++;
-
         } else {
-            imageIndex = 0;
+           imageIndex = 0;           
         }
         $(a[imageIndex]).fadeIn(2000).fadeOut(2000);       
         mainTimer = setTimeout(showImages, 4000);
@@ -41,9 +40,14 @@ $(document).ready(function () {
     });
     */
     if (($(window).width() > 300) && ($(window).width() < 500)) {
-          $(".buttons-container  > div").addClass("wrapped-in-buttons");      
+        $(" .buttons-container div p").css({ "font-size": "10px", "line-height": "80px" });
+        $(".buttons-container  > div").addClass("wrapped-in-buttons").css("width","281px");
         $(".buttons").addClass("resize-buttons-type-2");
-         $(".wrapped-in-buttons").css("left", n);
+        $(".wrapped-in-buttons").css("left", function (){
+            var b=(($(window).width()-$(".wrapped-in-buttons").width()))/2;
+         return b
+       
+        });
     } else if (($(window).width() > 500) && ($(window).width() < 1360)) {
         $(" .buttons-container div p").css({ "font-size": "10px", "line-height": "120px" });
         $(".buttons-container  > div").addClass("wrapped-in-buttons");
@@ -51,6 +55,6 @@ $(document).ready(function () {
         $(".wrapped-in-buttons").css("left", n);
     } else {
        $(".buttons-container  > div").addClass("wrapped-in-buttons").css("right","20px");      
-        console.log($(window).width());
+       
     }
 });
