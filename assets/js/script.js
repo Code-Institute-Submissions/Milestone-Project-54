84bfe89169a91f409729f8746b4eed870b29e78a
rimantascode <r.preiks@gmail.com>
2020-04-10 12:07:48 +0000

    changed layout in header and worked on resizing buttons, for smaller screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,12 @@ $(document).ready(function () {
     var a = [];
     var imageIndex = 0;
     var i = 0;
-    function middle(b) {
-        var a= (b-371)/2;
-       
-  
-        console.log(a);
-    }
     showImages();
     function showImages() {
         while (a.length < 3) {
             a[i] = document.getElementById("slider" + i);
             a[i].style.display = "none";
             i++;
-
         }
         if (imageIndex < a.length) {
             imageIndex++;
@@ -24,37 +17,16 @@ $(document).ready(function () {
         } else {
             imageIndex = 0;
         }
-        $(a[imageIndex]).fadeIn(2000).fadeOut(2000);
-        // $(a[0]).fadeIn(2000);
-
+        $(a[imageIndex]).fadeIn(2000).fadeOut(2000);       
         mainTimer = setTimeout(showImages, 4000);
     }
-
-    /*
-      $("header").mousemove(function (event) {
-          var movement= 100;
-         var movement=movement-event.PageX
-         
-          if (event.PageX == true ) {
-           
-                $(a[imageIndex]).stop();
-                $(a[imageIndex]).fadeIn();
-         console.log("moves");
-  
-          }else {
-            // setInterval(function(){ console.log("new"+movement); }, 3000);
-          }
-         
-      });
-      */
     var timeout = null;
-
     $(document).on('mousemove', function () {
         clearTimeout(timeout);
-        $(a[imageIndex]).stop().fadeIn(1000);
+        $(a[imageIndex]).stop().fadeIn(1000);    
         timeout = setTimeout(function () {
             console.log('Mouse idle for 3 sec');
-            clearTimeout(mainTimer);
+            clearTimeout(mainTimer);       
             showImages();
         }, 3000);
     });
@@ -69,31 +41,16 @@ $(document).ready(function () {
     });
     */
     if (($(window).width() > 300) && ($(window).width() < 500)) {
-          $(".buttons-container  > div").addClass("wrapped-in-buttons");
-        
+          $(".buttons-container  > div").addClass("wrapped-in-buttons");      
         $(".buttons").addClass("resize-buttons-type-2");
          $(".wrapped-in-buttons").css("left", n);
-
     } else if (($(window).width() > 500) && ($(window).width() < 1360)) {
-      
-       
-
-        // $( ".buttons-container div" ).css({"width":"70","height":"70","font-size":"10px"});
-
         $(" .buttons-container div p").css({ "font-size": "10px", "line-height": "120px" });
-        //   $(".buttons-container").css({"width":"120"});
-        //  $(".resize-buttons").css("bottom","20px");
         $(".buttons-container  > div").addClass("wrapped-in-buttons");
-        
         $(".buttons").addClass("resize-buttons-type-1");
         $(".wrapped-in-buttons").css("left", n);
-
     } else {
-
-        $(".buttons-container  > div").addClass("wrapped-in-buttons").css("right","20px");
-
-        /// $(".buttons ").css("width","100%");
+       $(".buttons-container  > div").addClass("wrapped-in-buttons").css("right","20px");      
         console.log($(window).width());
     }
-
 });
