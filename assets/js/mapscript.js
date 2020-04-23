a2a45f397e28257e9d60bf034728b50988d3e533
rimantascode <r.preiks@gmail.com>
2020-04-23 16:49:55 +0000

    conneted the buttons from top bar, now the user can select to what city to go and what and choose what places can visit from given option

--- assets/js/mapscript.js
+++ assets/js/mapscript.js
@@ -2,43 +2,61 @@
 var map;
 var service;
 var infowindow;
+var city;
+var visit;
+//var kingston = new google.maps.LatLng(18.020067, -76.796858);
+//  var moraineLakeCoo= new google.maps.LatLng(51.322047,-116.185993);
+//  var sognefjordCoo= new google.maps.LatLng(61.170106, 6.581191);
 
 function initMap() {
-    var kingstonCoo = new google.maps.LatLng(18.020067, -76.796858);
-    var moraineLakeCoo= new google.maps.LatLng(51.322047,-116.185993);
-    var sognefjordCoo= new google.maps.LatLng(61.170106, 6.581191);
-    var kingston= $("input[name='kingston']" ).val();
 
-  $("input[type='radio']").on("change", function () {
-    alert(this.value);
-});
-    infowindow = new google.maps.InfoWindow();
+    //  var kingston= $("input[name='kingston']" ).val();
 
-    map = new google.maps.Map(
-        document.getElementById('map'), { center: kingstonCoo, zoom: 15 });
-
-    var request = {
-        location: kingstonCoo,
-        radius: '500',
-        query: 'hotels'
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
-            map.setCenter(results[0].geometry.location);
+    $("input[type='radio']").on("change", function () {
+        if (this.value == "kingston") {
+            var city = new google.maps.LatLng(18.020067, -76.796858);
+        } else if (this.value == "Moraine-Lake") {
+            var city = new google.maps.LatLng(51.322047, -116.185993);
+        } else if (this.value == "Sognefjord") {
+            var city = new google.maps.LatLng(61.170106, 6.581191);
         }
-    }); 
+        $("input[type='radio']").change(function () {
+            infowindow = new google.maps.InfoWindow();
+            map = new google.maps.Map(
+                document.getElementById('map'), { center: city, zoom: 12 });
+            if (this.value == "hotels") {
+                // $("input[value='restaurants']").prop( "disabled", true );
+                //  $("input[value='beach']").prop( "disabled", true );
+                var visit = "hotels";
+            } else if (this.value == "restaurants") {
+                var visit = "restaurants";
+            } else if (this.value == "beach") {
+                var visit = "beach";
+            }
+            var request = {
+                location: city,
+                radius: '500',
+                query: visit
+            };
+            service = new google.maps.places.PlacesService(map);
+            service.textSearch(request, function (results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        createMarker(results[i]);
+                    }
+                    map.setCenter(results[0].geometry.location);
+                }
+            });
+        });
+    });
 }
+
+
 function createMarker(place) {
     var marker = new google.maps.Marker({
         map: map,
         position: place.geometry.location
     });
-
-
     function isItOpen() {
         if (place.opening_hours.open_now == false) {
             return '<h6 style="text-align:center;margin-top:3px;color:red;">Closed</h6';
@@ -53,6 +71,5 @@ function createMarker(place) {
         );
         infowindow.open(map, this);
     });
-
 }
 
