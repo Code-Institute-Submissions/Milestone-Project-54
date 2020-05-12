3b43161486a8dfc1233e3ff58087dc1533982cc1
rimantascode <r.preiks@gmail.com>
2020-05-12 10:39:57 +0000

    implemented emailjs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,7 +2,7 @@ function sendEmail(contactForm) {
      emailjs.send("r_preiks_gmail_com", "holiday", 
      {"from_name":contactForm.name.value,
         "from_email":contactForm.email.value,
-        "holiday_requist":contactForm.textarea.value
+        "holiday_request":contactForm.textarea.value
     })
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
